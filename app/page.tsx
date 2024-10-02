0612f2b12e76437d01181afe6bddf39b3231b13b
jhimi01 <talukderjhimi@gmail.com>
2024-10-02 21:14:08 +0600

    setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <main>
     <h1>hellow world</h1>
+    <Button>subscribe</Button>
    </main>
   );
 }
