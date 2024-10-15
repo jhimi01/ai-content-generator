ad06d203d49b4a49076e65c058e3acf9b8b375b0
jhimi01 <talukderjhimi@gmail.com>
2024-10-15 22:59:43 +0600

    backend setup done

---
+++ drizzle.config.js
@@ -0,0 +1,8 @@
+/** @type { import("drizzle-kit").Config } */
+export default {
+    schema: './utils/schema.tsx',
+    dialect: 'postgresql',
+    dbCredentials: {
+        url: 'postgresql://neondb_owner:6X3IYSkglEdN@ep-long-poetry-a56qazd8.us-east-2.aws.neon.tech/ai-content-genaretor?sslmode=require'
+    }
+}
