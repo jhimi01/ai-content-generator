bbca1d66c4c998b193d262a22dc5cc513599bc28
jhimi01 <talukderjhimi@gmail.com>
2024-10-15 22:27:28 +0600

    backend setup

---
+++ utils/db.tsx
@@ -0,0 +1,4 @@
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DB_URL!);
+export const db = drizzle(sql);
