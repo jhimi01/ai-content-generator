f9a79ad2f240e83c4f29415d3ce1a60e74905d1e
jhimi01 <talukderjhimi@gmail.com>
2024-10-18 23:26:19 +0600

    added loading state

--- app/dashboard/loading.js
+++ app/dashboard/loading.js
@@ -1,23 +1,21 @@
-'use client'
-
+import Image from 'next/image';
 import React from 'react';
-import Lottie from 'react-lottie';
-// import animationData from '/loadanimation.json'; // Adjust the path as needed
+
 
 export default function Loading() {
-  const defaultOptions = {
-    loop: true, // Set to true for looping the animation
-    autoplay: true, // Set to true to start the animation automatically
-    animationData: '/loadanimation.json',
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
-  };
+ 
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <Lottie options={defaultOptions} height={400} width={400} />
-      <p>This is loading 🔃🔃🔃🍞🍞💕💕💕💕</p>
+      <Image
+          src={"/aibanner.png"}
+          alt="banner"
+          width={300}
+          height={300}
+          className=""
+        />
+      <p className='text-2xl font-bold text-gray-500'>Wait for a while 🔃🔃🔃🍞🍞💕💕💕💕</p>
+     
     </div>
   );
 }
